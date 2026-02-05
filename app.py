c552ae7f2d35e9e3e69e4059f96f2e09e7bd4b00
awbasit <awbasit99@gmail.com>
2026-02-05 00:17:40 +0000

    Project Structure

---
+++ app.py
@@ -0,0 +1,5 @@
+import os
+import gradio as gr
+from openai import OpenAI
+
+op
---
+++ services/llm_client.py
@@ -0,0 +1,9 @@
+import os
+from openai import OpenAI
+
+
+def llm_client():
+    try:
+        a = 1+1
+    except:
+        pass
